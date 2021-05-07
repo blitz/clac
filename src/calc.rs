5650c2dac42a05734869f94d9eb167533493536f
Julian Stecklina <js@alien8.de>
2021-05-07 14:24:29 +0200

    Implement missing arithmetic operations

--- src/calc.rs
+++ src/calc.rs
@@ -4,6 +4,8 @@
 //! stack-based calculator works somewhat like a classic [HP
 //! 48](https://en.wikipedia.org/wiki/HP_48_series) calculator.
 
+use std::convert::TryInto;
+
 use crate::types::{Operation, Value};
 
 /// All errors that happen during calculation are represented by this
@@ -33,35 +35,174 @@ pub struct Calculator {
     value_stack: Vec<Value>,
 }
 
-fn any_is_float(a: Value, b: Value) -> bool {
-    a.is_float() || b.is_float()
+/// A generic type for all kinds of calculator operation
+/// implementations.
+trait OpImplementation {
+    fn execute(&self, calc: &mut Calculator) -> Result<(), CalculatorError>;
 }
 
-fn add(a: Value, b: Value) -> Result<Value, CalculatorError> {
-    if any_is_float(a, b) {
-        Ok(Value::Float(f64::from(a) + f64::from(b)))
-    } else {
-        Ok(Value::Integer(
-            i64::from(a)
-                .checked_add(b.into())
-                .ok_or(CalculatorError::InvalidOperation)?,
-        ))
+struct PushImplementation {
+    value: Value,
+}
+
+impl PushImplementation {
+    fn new(value: Value) -> Self {
+        PushImplementation { value }
     }
 }
 
-fn divide(a: Value, b: Value) -> Result<Value, CalculatorError> {
-    if any_is_float(a, b) {
-        Ok(Value::Float(f64::from(a) / f64::from(b)))
-    } else {
-        Ok(Value::Integer(
-            i64::from(a)
-                .checked_div(b.into())
-                .ok_or(CalculatorError::InvalidOperation)?,
-        ))
+impl OpImplementation for PushImplementation {
+    fn execute(&self, calc: &mut Calculator) -> Result<(), CalculatorError> {
+        calc.push_mut(self.value);
+
+        Ok(())
+    }
+}
+
+trait TwoParamOpImplementation {
+    fn compute(&self, a: Value, b: Value) -> Result<Value, CalculatorError>;
+}
+
+impl<T: TwoParamOpImplementation> OpImplementation for T {
+    fn execute(&self, calc: &mut Calculator) -> Result<(), CalculatorError> {
+        let b = calc.pop_mut()?;
+        let a = calc.pop_mut()?;
+
+        Ok(calc.push_mut(self.compute(a, b)?))
+    }
+}
+
+/// A two parameter operation that promotes both its arguments to
+/// float, if any one of it is.
+struct FloatPromotingOp2 {
+    int_op: Box<dyn Fn(i64, i64) -> Result<Value, CalculatorError>>,
+    float_op: Box<dyn Fn(f64, f64) -> Result<Value, CalculatorError>>,
+}
+
+impl FloatPromotingOp2 {
+    fn new(
+        int_op: impl Fn(i64, i64) -> Result<Value, CalculatorError> + 'static,
+        float_op: impl Fn(f64, f64) -> Result<Value, CalculatorError> + 'static,
+    ) -> Self {
+        FloatPromotingOp2 {
+            int_op: Box::new(int_op),
+            float_op: Box::new(float_op),
+        }
+    }
+}
+
+impl TwoParamOpImplementation for FloatPromotingOp2 {
+    fn compute(&self, a: Value, b: Value) -> Result<Value, CalculatorError> {
+        if a.is_float() || b.is_float() {
+            (self.float_op)(a.into(), b.into())
+        } else {
+            (self.int_op)(a.into(), b.into())
+        }
+    }
+}
+
+/// A two parameter operation that promotes both its arguments to
+/// integers all the time.
+struct IntPromotingOp2 {
+    int_op: Box<dyn Fn(i64, i64) -> Result<Value, CalculatorError>>,
+}
+
+impl IntPromotingOp2 {
+    fn new(int_op: impl Fn(i64, i64) -> Result<Value, CalculatorError> + 'static) -> Self {
+        IntPromotingOp2 {
+            int_op: Box::new(int_op),
+        }
+    }
+}
+
+impl TwoParamOpImplementation for IntPromotingOp2 {
+    fn compute(&self, a: Value, b: Value) -> Result<Value, CalculatorError> {
+        (self.int_op)(i64::from(a), i64::from(b))
+    }
+}
+
+impl From<Operation> for Box<dyn OpImplementation> {
+    fn from(op: Operation) -> Self {
+        match op {
+            Operation::Add => Box::new(FloatPromotingOp2::new(
+                |a, b| -> Result<Value, CalculatorError> {
+                    Ok(Value::Integer(
+                        a.checked_add(b).ok_or(CalculatorError::InvalidOperation)?,
+                    ))
+                },
+                |a, b| -> Result<Value, CalculatorError> { Ok(Value::Float(a + b)) },
+            )),
+
+            Operation::BitAnd => Box::new(IntPromotingOp2::new(
+                |a, b| -> Result<Value, CalculatorError> { Ok(Value::Integer(a & b)) },
+            )),
+
+            Operation::BitOr => Box::new(IntPromotingOp2::new(
+                |a, b| -> Result<Value, CalculatorError> { Ok(Value::Integer(a | b)) },
+            )),
+
+            Operation::BitXor => Box::new(IntPromotingOp2::new(
+                |a, b| -> Result<Value, CalculatorError> { Ok(Value::Integer(a ^ b)) },
+            )),
+
+            Operation::Divide => Box::new(FloatPromotingOp2::new(
+                |a, b| -> Result<Value, CalculatorError> {
+                    Ok(Value::Integer(
+                        a.checked_div(b).ok_or(CalculatorError::InvalidOperation)?,
+                    ))
+                },
+                |a, b| -> Result<Value, CalculatorError> { Ok(Value::Float(a / b)) },
+            )),
+
+            Operation::LeftShift => Box::new(IntPromotingOp2::new(
+                |a, b| -> Result<Value, CalculatorError> {
+                    Ok(Value::Integer(
+                        a.checked_shl(
+                            b.try_into()
+                                .map_err(|_| CalculatorError::InvalidOperation)?,
+                        )
+                        .ok_or(CalculatorError::InvalidOperation)?,
+                    ))
+                },
+            )),
+
+            Operation::Multiply => Box::new(FloatPromotingOp2::new(
+                |a, b| -> Result<Value, CalculatorError> {
+                    Ok(Value::Integer(
+                        a.checked_mul(b).ok_or(CalculatorError::InvalidOperation)?,
+                    ))
+                },
+                |a, b| -> Result<Value, CalculatorError> { Ok(Value::Float(a * b)) },
+            )),
+
+            Operation::Subtract => Box::new(FloatPromotingOp2::new(
+                |a, b| -> Result<Value, CalculatorError> {
+                    Ok(Value::Integer(
+                        a.checked_sub(b).ok_or(CalculatorError::InvalidOperation)?,
+                    ))
+                },
+                |a, b| -> Result<Value, CalculatorError> { Ok(Value::Float(a - b)) },
+            )),
+
+            Operation::RightShift => Box::new(IntPromotingOp2::new(
+                |a, b| -> Result<Value, CalculatorError> {
+                    Ok(Value::Integer(
+                        a.checked_shr(
+                            b.try_into()
+                                .map_err(|_| CalculatorError::InvalidOperation)?,
+                        )
+                        .ok_or(CalculatorError::InvalidOperation)?,
+                    ))
+                },
+            )),
+
+            Operation::Push(v) => Box::new(PushImplementation::new(v)),
+        }
     }
 }
 
 impl Calculator {
+    #[allow(dead_code)]
     pub fn new() -> Self {
         Self::default()
     }
@@ -76,6 +217,7 @@ impl Calculator {
         self.value_stack.push(v)
     }
 
+    #[allow(dead_code)]
     pub fn push(&self, v: Value) -> Calculator {
         let mut new_calc = self.clone();
 
@@ -85,28 +227,12 @@ impl Calculator {
 
     /// Apply a single operation on the calculator.
     pub fn apply_mut(&mut self, op: Operation) -> Result<(), CalculatorError> {
-        match op {
-            Operation::Push(v) => self.push_mut(v),
-            Operation::Add => {
-                let b = self.pop_mut()?;
-                let a = self.pop_mut()?;
-
-                self.push_mut(add(a, b)?);
-            }
-            Operation::Divide => {
-                let b = self.pop_mut()?;
-                let a = self.pop_mut()?;
-
-                self.push_mut(divide(a, b)?);
-            }
-            _ => unimplemented!("{:?}", op),
-        }
-
-        Ok(())
+        Box::<dyn OpImplementation>::from(op).execute(self)
     }
 
     /// A side-effect free version of [apply_mut] that returns a new
     /// calculator with the result.
+    #[allow(dead_code)]
     pub fn apply(&self, op: Operation) -> Result<Self, CalculatorError> {
         let mut new_calc = self.clone();
 
