5f2a65523bb883b7377dea9378e4849a35883b77
Julian Stecklina <js@alien8.de>
2022-09-21 23:31:47 +0200

    Remove unused flake-compat input

--- flake.nix
+++ flake.nix
@@ -5,40 +5,30 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     naersk.url = "github:nix-community/naersk";
     utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-    flake-compat-ci.url = "github:hercules-ci/flake-compat-ci";
   };
 
-  outputs = { self, utils, nixpkgs, naersk, flake-compat, flake-compat-ci }:
-    (utils.lib.eachDefaultSystem (system: let
+  outputs = { self, utils, nixpkgs, naersk }:
+    (utils.lib.eachSystem [
+      "x86_64-linux"
+    ] (system: let
       pkgs = nixpkgs.legacyPackages."${system}";
       naersk-lib = naersk.lib."${system}";
+
+      clac = naersk-lib.buildPackage {
+        pname = "clac";
+        root = ./.;
+      };
     in rec {
       # `nix build`
       packages = {
-        clac = naersk-lib.buildPackage {
-          pname = "clac";
-          root = ./.;
-        };
-      };
+        inherit clac;
 
-      defaultPackage = packages.clac;
+        default = clac;
+      };
 
       # `nix run`
       apps.clac = utils.lib.mkApp {
         drv = packages.clac;
       };
-      defaultApp = apps.clac;
-    })) // {
-      # For Hercules CI, which doesn't natively support flakes (yet).
-      ciNix = flake-compat-ci.lib.recurseIntoFlakeWith {
-        flake = self;
-
-        # Optional. Systems for which to perform CI.
-        systems = [ "x86_64-linux" ];
-      };
-    };
+    }));
 }
